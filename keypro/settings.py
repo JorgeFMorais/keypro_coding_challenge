c3b5109d414fcd620765cf62eacce1ddc9fe6fa3
JorgeFMorais <jorgemorais1993@hotmail.com>
2024-10-01 15:51:29 +0000

    Add UserSerializer and UserViewSet for API endpoint

--- keypro/settings.py
+++ keypro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "keypro.wsgi.application"
+
+
+# REST Framework settings
+# https://www.django-rest-framework.org/api-guide/settings/
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 
 # Database

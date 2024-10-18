00b2bc42c59a8edb2cd59b47620b0cbaa49dd710
JorgeFMorais <jorgemorais1993@hotmail.com>
2024-10-18 14:47:46 +0000

    Update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard.tsx';
 import Login from './components/Login.tsx'
+import Register from './components/Register.tsx';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-/*
-  This example requires some changes to your config:
-*/
-export default function Login() {
+export default function Register() {
     const [formData, setFormData] = useState({
-      email: '',
-      password: '',
+        username: '',
+        email: '',
+        password1: '',
+        password2: '',
     });
 
     const handleChange = (e) => {
-      setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-      });
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
     };
 
     const [isLoading, setIsLoading] = useState(false);
@@ -33,13 +32,11 @@ export default function Login() {
         setErrorMessage(null)
 
         try {
-            const response = await axios.post('http://localhost:8000/api/login/', formData)
+            const response = await axios.post('http://localhost:8000/api/register/', formData)
             console.log("Success", response)
-            setSuccessMessage("Login successful")
-            localStorage.setItem('accessToken', response.data.tokens.access)
-            localStorage.setItem('refreshToken', response.data.tokens.refresh)
+            setSuccessMessage("Registration successful")
         } catch (error) {
-            console.error("Error during login", error.response?.data)
+            console.error("Error during registration", error.response?.data)
             if (error.response && error.response.data) {
                 Object.keys(error.response.data).forEach((key) => {
                     const errorMessages = error.response.data[key]
@@ -55,7 +52,7 @@ export default function Login() {
     };
 
     return (
-      <>
+        <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
@@ -64,12 +61,13 @@ export default function Login() {
               className="mx-auto h-10 w-auto"
             />
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Login to Points of Interest
+              Create an Account
             </h2>
-          </div>
+          </div>       
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" className="space-y-6">
+              
               { 
                 errorMessage 
                 && 
@@ -79,7 +77,25 @@ export default function Login() {
                         {errorMessage}
                     </p>
                 </div>
-              }    
+              }     
+
+              <div>
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    required
+                    autoComplete="username"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formData.username}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                   Email address
@@ -106,13 +122,33 @@ export default function Login() {
                 </div>
                 <div className="mt-2">
                   <input
-                    id="password"
-                    name="password"
+                    id="password1"
+                    name="password1"
                     type="password"
                     required
                     autoComplete="current-password"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    value={formData.password}
+                    value={formData.password1}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password2"
+                    name="password2"
+                    type="password"
+                    required
+                    autoComplete="current-password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formData.password2}
                     onChange={handleChange}
                   />
                 </div>
@@ -125,20 +161,19 @@ export default function Login() {
                   onClick={handleSubmit}
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Login
+                  Register
                 </button>
               </div>
             </form>
   
             <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Register
+              Already a member?{' '}
+              <a href="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                Sign in here
               </a>
             </p>
           </div>
         </div>
       </>
     )
-  }
-  
+}
